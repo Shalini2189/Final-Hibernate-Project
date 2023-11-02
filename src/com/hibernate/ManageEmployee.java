8aa652ebce7cecb7dc953bd16018eb2e835aacff
Chalu <shalini218099@gmail.com>
2023-11-02 18:43:02 +0530

    First commit

--- src/com/hibernate/ManageEmployee.java
+++ src/com/hibernate/ManageEmployee.java
@@ -25,31 +25,59 @@ public class ManageEmployee
 	// creating onjects
 	ManageEmployee ME = new ManageEmployee();
 	Integer emp1 = ME.addEmployee("Lily","lil","Developer",25000);
+	Integer emp2 = ME.addEmployee("Asha","S","Team head",65000);
+	Integer emp3 = ME.addEmployee("Yeshu","Y","Admin",35000);
+	Integer emp4 = ME.addEmployee("Shali","S","Developer",45000);
+	Integer emp5 = ME.addEmployee("Sham","H","Manager",95000);
+	Integer emp6 = ME.addEmployee("Sonu","N","Developer",35000);
+	
 	
 	ME.listEmployee();
-	ME.updateEmployee(emp1,50000);
+	ME.updateEmployee(emp1,45000);
+	ME.listEmployee();
+	ME.deleteEmployee(emp5);
+}
+public void deleteEmployee(Integer emp)
+{
+	Session s = sf.openSession();
+	Transaction tx = null;
+	try
+	{
+		tx = s.beginTransaction();
+		Employee empdata = s.get(Employee.class, emp);
+		s.delete(empdata);
+		tx.commit();
+	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+public void updateEmployee(Integer e1,double sal)
+{
+	Session s = sf.openSession();
+	Transaction tx = null;
+	try
+	{
+		
+		tx = s.beginTransaction();
+		Employee emp = s.get(Employee.class, e1);
+		emp.setSalary(sal);
+		s.update(emp);
+		tx.commit();
+	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+
+public void listEmployee()
+{
+	Session s = sf.openSession();
+	Transaction tx = null;
 	
-	}
-	public void updateEmployee(Integer e1,double sal)
-	{
-		Session s =sf.openSession();
-		Transaction tx = null;
-		try
-		{
-			tx = s.beginTransaction();
-			Employee emp = s.get(Employee.class,e1);
-			
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-	public void listEmployee()
-	{
-		Session s =sf.openSession();
-		Transaction tx = null;
-		try
+	try
 		{
 			tx = s.beginTransaction();
 			Query q =s.createQuery("from Employee");
